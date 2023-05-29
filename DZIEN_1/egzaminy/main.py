df24e9ee27a24f10ea801dbac2c46abfef4bd3e3
albim72 <marcin.albiniak72@gmail.com>
2023-05-29 15:45:43 +0200

    Update main.py

--- DZIEN_1/egzaminy/main.py
+++ DZIEN_1/egzaminy/main.py
@@ -1,5 +1,13 @@
-from homework import Homework
-
 st = Homework()
 st.grade = 95
 assert st.grade == 95
+
+print("______________________")
+
+st1 = Exam()
+st1.writing_grade = 120
+st1.math_grade = 67
+
+print(f'oceny -> writing: {st1.writing_grade}, math: {st1.math_grade}')
+assert st1.writing_grade == 88
+assert st1.math_grade == 67
