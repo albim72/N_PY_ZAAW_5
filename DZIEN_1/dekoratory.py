1229d87cc27ee296e9b53821cbb962d2e9fea38a
albim72 <marcin.albiniak72@gmail.com>
2023-05-29 11:41:46 +0200

    Update dekoratory.py

--- DZIEN_1/dekoratory.py
+++ DZIEN_1/dekoratory.py
@@ -21,3 +21,17 @@ def big_lista(n):
     sum([(i+1)**2 for i in range(n)]) #list comprehension
 
 big_lista(13_000_000)
+
+#przykład2
+
+def debug(funkcja):
+    def wrapper(*args):
+        print(f'wołana funkcja to: {funkcja.__name__}')
+        funkcja(*args)
+    return wrapper
+
+@debug
+def info(msg):
+    print(f'ważna informacja: {msg}')
+
+info("takie tam")
