b222cb876af4e313f1084277939c271a3f66b621
albim72 <marcin.albiniak72@gmail.com>
2023-05-29 11:37:57 +0200

    Update dekoratory.py

--- DZIEN_1/dekoratory.py
+++ DZIEN_1/dekoratory.py
@@ -1,15 +1,23 @@
 import time
 #przykład1
 def pomiarczasu(funkcja):
-    def wrapper():
+    def wrapper(*args):
         starttime = time.perf_counter()
-        funkcja()
+        funkcja(*args)
         endtime = time.perf_counter()
         print(f"czas wykonania funckji: {endtime-starttime} s")
     return wrapper
 
-@pomiarczasu
-def big_lista():
-    sum([(i+1)**2 for i in range(10_000_000)]) #list comprehension
+def sleep(funkcja):
+    def wrapper(*args):
+        time.sleep(2)
+        return funkcja(*args)
+    return wrapper
 
-big_lista()
+
+@sleep
+@pomiarczasu
+def big_lista(n):
+    sum([(i+1)**2 for i in range(n)]) #list comprehension
+
+big_lista(13_000_000)
