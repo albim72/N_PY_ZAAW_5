24d21b57b9781bb1c077396e45c11878298054ce
albim72 <marcin.albiniak72@gmail.com>
2023-05-31 10:17:38 +0200

    Update main.py

--- DZIEN_3/game_environment/main.py
+++ DZIEN_3/game_environment/main.py
@@ -2,18 +2,18 @@
 class Frog:
     def __init__(self,name):
         self.name = name
-        
+
     def __str__(self):
         return self.name
-    
+
     def interact_with(self,obstacle):
         act = obstacle.action()
         msg = f'Żaba {self} spotyka {obstacle} i {act}!'
-        
+
 class Bug:
     def __str__(self):
         return 'robaka'
-    
+
     def action(self):
         return 'zjada go'
     
@@ -21,12 +21,48 @@ class FrogWorld:
     def __init__(self,name):
         print(self)
         self.player_name = name
-        
+
     def __str__(self):
         return '\n\n\t--------- Frog World ----------'
 
     def make_character(self):
         return Frog(self.player_name)
-    
+
     def make_obstacle(self):
         return Bug()
+
+
+# Gra Czarnoksiężnika
+class Warlock:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def interact_with(self, obstacle):
+        act = obstacle.action()
+        msg = f'Czarnoksiężnik {self} walczy ze {obstacle} i {act}!'
+
+
+class Ork:
+    def __str__(self):
+        return 'złym orkiem'
+
+    def action(self):
+        return 'masakruje go'
+
+
+class WorlockWorld:
+    def __init__(self, name):
+        print(self)
+        self.player_name = name
+
+    def __str__(self):
+        return '\n\n\t--------- Warlock World ----------'
+
+    def make_character(self):
+        return Warlock(self.player_name)
+
+    def make_obstacle(self):
+        return Ork()
