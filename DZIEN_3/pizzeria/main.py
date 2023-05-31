a9481f1e894f056e0ab6215ff747332ce9a76e72
albim72 <marcin.albiniak72@gmail.com>
2023-05-31 09:31:54 +0200

    Update main.py

--- DZIEN_3/pizzeria/main.py
+++ DZIEN_3/pizzeria/main.py
@@ -19,15 +19,43 @@ class Pizza:
         self.dough = None
         self.sauce = None
         self.topping = []
-        
+
     def __str__(self):
         return self.name
-    
+
     def prepare_dough(self,dough):
         self.dough = dough
         print(f'preparing the {self.dough.name} dough of your {self} ... ')
         time.sleep(STEP_DELAY)
         print(f'done with the {self.dough.name} dough')
-    
-    
+
+class MargaritaBuilder:
+    def __init__(self):
+        self.pizza = Pizza('margarita')
+        self.progress = PizzaProgress.queued
+        self.baking_time = 5
         
+    def prepare_dough(self):
+        self.progress = PizzaProgress.preparation
+        self.pizza.prepare_dough(PizzaDough.thin)
+        
+    def add_sauce(self):
+        print('adding the tomato sauce to your margarita...')
+        self.pizza.sauce = PizzaSauce.tomato
+        time.sleep(STEP_DELAY)
+        print('done with the tomato sauce...')
+        
+    def add_topping(self):
+        topping_desc ='double mozarella, oregano'
+        topping_items = (PizzaTopping.double_mozarella,PizzaTopping.oregano)
+        print(f'adding the topping ({topping_desc}) to your margarita...')
+        self.pizza.topping.append([t for t in topping_items])
+        time.sleep(STEP_DELAY)
+        print(f'dane with the topping ({topping_desc})')
+        
+    def bake(self):
+        self.progress = PizzaProgress.baking
+        print(f'bakieng your margarita for {self.baking_time} s')
+        time.sleep(self.baking_time)
+        self.progress = PizzaProgress.ready
+        print('Your margarita is ready!!!')
